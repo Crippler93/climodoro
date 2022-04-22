1971d7c5be9fad187ee8e295baea16bb3b38678a
Luis Chavez <luischavezcald@gmail.com>
2022-04-22 08:59:05 -0400

    moved ascii text to different module

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import inquirer from 'inquirer';
+import art from './asciiArt';
 
-const menuOptions = ['Start tracking time', 'View time log', 'Exit']
-const trackingOptions = ['Back to menu', 'Exit']
+const menuOptions = ['Start tracking time', 'View time log', 'Exit'];
+const trackingOptions = ['Back to menu', 'Exit'];
+let intervalID = null;
 
 async function showMenu() {
   const answers = await inquirer.prompt(
@@ -15,7 +17,7 @@ async function showMenu() {
         ],
 
       }
-    ])
+    ]);
   if (answers.theme === menuOptions[0]) {
     await showTrackingMenu();
   }
@@ -24,17 +26,19 @@ async function showMenu() {
 
 
 async function showTrackingMenu() {
-console.log(`
-       _                _                           _     _               
-      | |              | |                         | |   (_)              
-  ___ | |_  __ _  _ __ | |_  __      __ ___   _ __ | | __ _  _ __    __ _ 
- / __|| __|/ _\` || '__|| __| \\ \\ /\\ / // _ \\ | '__|| |/ /| || '_ \  / _\` |
- \\__ \\| |_| (_| || |   | |_   \\ V  V /| (_) || |   |   < | || | | || (_| |
- |___/ \\__|\\__,_||_|    \\__|   \\_/\\_/  \\___/ |_|   |_|\\_\\|_||_| |_| \\__, |
-                                                                     __/ |
-                                                                    |___/ 
-`);
+  art.startWorking();
   let time = new Date().getTime();
+  let counter = 0;
+
+  function printProgress() {
+    process.stdout.cursorTo(0);
+    process.stdout.write('Count is: ' + counter);
+    counter++;
+  }
+  intervalID = setInterval(function() {
+    printProgress();
+  }, 1000);
+
   const answers = await inquirer.prompt(
     [
       {
@@ -46,11 +50,20 @@ console.log(`
         ],
 
       }
-    ])
-    console.log(JSON.stringify(answers, null, '  '));
-    const difference = new Date().getTime() - time;
-    const [, hours, minutes, seconds ] = /(\d\d):(\d\d):(\d\d)/.exec(new Date(difference).toUTCString()) as string[]
-    console.log("Time spent", `${hours}:${minutes}:${seconds}`);
+    ]
+  );
+
+  const difference = new Date().getTime() - time;
+  const [, hours, minutes, seconds ] = /(\d\d):(\d\d):(\d\d)/.exec(new Date(difference).toUTCString()) as string[];
+  console.log("Time spent", `${hours}:${minutes}:${seconds}`);
+  clearInterval(intervalID);
+  intervalID = null;
+
+  if (answers.theme === trackingOptions[0]) {
+    await showMenu();
+  } else {
+    process.exit(0);
+  }
     
 }
 
@@ -58,13 +71,5 @@ async function showTimeLog() {
   
 }
 
-console.log(`%c
- _____   _   _                           _                        
-/ ____| | | (_)                         | |                       
-| |     | |  _   _ __ ___     ___     __| |   ___    _ __    ___  
-| |     | | | | | '_ \` _ \\   / _ \\   / _\` |  / _ \\  | '__|  / _ \\ 
-| |____ | | | | | | | | | | | (_) | | (_| | | (_) | | |    | (_) |
-\\_____| |_| |_| |_| |_| |_|  \\___/   \\__,_|  \\___/  |_|     \\___/                                         
-`);
-
+art.climodoro();
 showMenu();
